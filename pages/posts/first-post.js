1401af3a4b0f74c3c5f05f6c6beef8dc803f7bb2
sumit45doc <ss8559283@gmail.com>
2021-06-16 21:58:21 +0530

    post gobal css test

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import Head from "next/head"
+import Layout from "../../components/Layout"
+import Post from "../../secondaryComponent/Post"
+
 function firstpost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First post</title>
             </Head>
@@ -14,8 +17,9 @@ function firstpost() {
                    <a>Back to home</a>
                 </Link>
             </h2>
-            <style jsx>{`.first{color: blue}`}</style>
-        </>
+            <Post/>
+            {/* <style jsx>{`.first{color: blue}`}</style> */}
+        </Layout>
     )
 }
 
---
+++ secondaryComponent/Post.js
@@ -0,0 +1,10 @@
+
+function Post() {
+    return (
+        <div>
+<h1 className="post">From post</h1>            
+        </div>
+    )
+}
+
+export default Post
